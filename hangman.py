f002ca0130314132825d1a900b3576415bd7fdfd
Suzanne Rosenzweig <srosenzweig09@gmail.com>
2020-03-24 09:13:15 -0400

    Modified the game so the letters in the word are hidden but appear when you guess the correct letter
    umm
    alskdjf
    
    .q
    :x

--- hangman.py
+++ hangman.py
@@ -8,11 +8,13 @@ def drawLimb():
     print('Bad choice. :(')
 
 def checkLetter(index):
-    matches = []
+    f = True
     for i,letter in enumerate(word):
         if letter == letters[index]:
-            matches.append(i)
-    if len(matches) == 0:
+            #word_buttons[word_dic[letters[index]]].grid(row=0, column=word_dic[letters[index]])
+            word_buttons[word_dic[letters[index]]].config(text=letter)
+            f = False
+    if f:
         drawLimb()
 
 def clickLetter(index):
@@ -53,25 +55,50 @@ spacer2.grid(row = 5)
 #f = tk.Frame()
 #f.grid(row=0, column=0, rowspan=3, columnspan=10)
 
-#a = tk.Text()
-#a.grid(c,row=0, column=0, rowspan=3, columnspan=10)
+# a = tk.Text(c, state="disabled")
+# a.grid(row=0, column=0, columnspan=10)
+# a.insert(tk.INSERT, 'Word!')
 
 # Generate random words and draw one.
 rw = RandomWords()
-x = 200
-y= x + 10
-word = rw.get_random_word().upper()
-print(word)
 
+def generate_word():
+    word = rw.get_random_word().upper()
+    for i in word:
+        if i == '-' or i == ' ':
+            generate_word()
+    return word
+
+word = generate_word()
+print(word)
+word_dic = {}
+
+size = 1
+word_buttons = []
+for index in range(len(word)):
+    word_dic[word[index]] = index
+    letter = word[index]
+    word_button = tk.Button(top_frame, height=size, width=size, highlightbackground='black')
+    word_button.grid(padx=3,pady=1, row=0, column=index)
+    #word_button.grid_forget()
+    word_buttons.append(word_button)
+
+
+
+x1 = 135
+x2 = x1 + 10
+y = 400
 for i in range(len(word)):
-    c.create_line(x,250,y,250)
-    x+=15
-    y+=15
+    c.create_line(x1,y,x2,y)
+    x1+=15
+    x2+=15
 
 size = 1
 buttons = []
+dic = {}
 for index in range(len(letters)):
     letter = letters[index]
+    dic[letters[index]] = index
     button = tk.Button(bottom_frame, text=letter, command=lambda index=index, n=letter: clickLetter(index), height=size, width=size)
     button.grid(padx=3,pady=1, row=int(np.floor(index/10)+3), column=index%10)
     buttons.append(button)
