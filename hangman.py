82847687d2c24c17f7e70d90578f7e415880cd84
Suzanne Rosenzweig <srosenzweig09@gmail.com>
2020-03-24 10:11:22 -0400

    Committing before I add __init__ and __main__ functions. I have found that I cannot keep a count of how many limbs should exist until I separate the main loop from the initialization.

--- hangman.py
+++ hangman.py
@@ -4,23 +4,37 @@ from random_word import RandomWords
 
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-def drawLimb():
-    print('Bad choice. :(')
+### Functions are defined here! ###
+def drawLimb(count):
+    """Makes a new limb appear if the user guesses an incorrect letter."""
+    # Create hangman
+    print(count)
+    if count == 1:
+        c.create_oval(180,150,220,190) # Head
+    elif count == 2:
+        c.create_line(200,190,200,250) # Torso
+    elif count == 3:
+        c.create_line(200,200,220,230) # Right arm
+    elif count == 4:
+        c.create_line(200,200,180,230) # Left arm
+    elif count == 5:
+        c.create_line(200,250,185,280) # Left leg
+    elif count == 6:
+        c.create_line(200,250,215,280) # Right leg
+    
 
-def checkLetter(index):
+def checkLetter(index,count):
+    """Checks the letter that the user guesses to see if it is correct."""
+    buttons[index].config(state="disabled")
     f = True
     for i,letter in enumerate(word):
         if letter == letters[index]:
-            #word_buttons[word_dic[letters[index]]].grid(row=0, column=word_dic[letters[index]])
-            word_buttons[word_dic[letters[index]]].config(text=letter)
+            word_buttons[i].config(text=letter)
             f = False
     if f:
-        drawLimb()
-
-def clickLetter(index):
-    checkLetter(index)
-    buttons[index].config(state="disabled")
-
+        print("I should be drawing...")
+        count+=1
+        drawLimb(count)
 
 
 # Create main window as master, 'm'.
@@ -30,40 +44,33 @@ m.title('Hangman Game')
 windowWidth = m.winfo_reqwidth()
 windowHeight = m.winfo_reqheight()
 # Gets both half the screen width/height and window width/height
-positionRight = int(m.winfo_screenwidth()/2 - windowWidth/2)
-positionDown = int(m.winfo_screenheight()/2 - windowHeight/2)
+positionRight = int(m.winfo_screenwidth()/3 - windowWidth/2)
+positionDown = int(m.winfo_screenheight()/3 - windowHeight/2)
 #m.geometry("+{}+{}".format(positionRight, positionDown))
-m.geometry("500x600")
+m.geometry("500x600+{}+{}".format(positionRight, positionDown))
 
 
 # Create a canvas on which to draw image of hangman.
-c = tk.Canvas(m, width=400, height=400)
+c = tk.Canvas(m, width=500, height=400)
 # Pack up the canvas (organizes in blocks)
 c.grid()
 
+# Create two different frames. The top will contain the hangman drawing and the hidden word. The bottom frame will contain the keyboard.
 top_frame = tk.Frame(m)
 top_frame.grid()
 bottom_frame = tk.Frame(m)
 bottom_frame.grid()
 
-#empty row spacers
+# Empty row spacers for aesthetic.
 spacer1 = tk.Label(top_frame, text = "")
 spacer1.grid(row = 4)
 spacer2= tk.Label(top_frame, text = "")
 spacer2.grid(row = 5)
 
-#f = tk.Frame()
-#f.grid(row=0, column=0, rowspan=3, columnspan=10)
-
-# a = tk.Text(c, state="disabled")
-# a.grid(row=0, column=0, columnspan=10)
-# a.insert(tk.INSERT, 'Word!')
-
 # Generate random words and draw one.
-rw = RandomWords()
-
 def generate_word():
-    word = rw.get_random_word().upper()
+    rw = RandomWords()
+    word = rw.get_random_word(hasDictionaryDef='true').upper()
     for i in word:
         if i == '-' or i == ' ':
             generate_word()
@@ -83,27 +90,27 @@ for index in range(len(word)):
     #word_button.grid_forget()
     word_buttons.append(word_button)
 
+# Create hangman's gallows.
+c.create_line(300,300,400,300)
+c.create_line(350,300,350,100)
+c.create_line(350,100,200,100)
+c.create_line(200,100,200,150)
 
 
-x1 = 135
-x2 = x1 + 10
-y = 400
-for i in range(len(word)):
-    c.create_line(x1,y,x2,y)
-    x1+=15
-    x2+=15
-
+# Create letter buttons
 size = 1
 buttons = []
 dic = {}
+count = 0
 for index in range(len(letters)):
     letter = letters[index]
     dic[letters[index]] = index
-    button = tk.Button(bottom_frame, text=letter, command=lambda index=index, n=letter: clickLetter(index), height=size, width=size)
+    button = tk.Button(bottom_frame, text=letter, command=lambda index=index, n=letter: checkLetter(index,count), height=size, width=size)
     button.grid(padx=3,pady=1, row=int(np.floor(index/10)+3), column=index%10)
     buttons.append(button)
 
-stop = tk.Button(m, text='Stop', height=size, width=25, command=m.destroy)
+# Create End Game button
+stop = tk.Button(m, text='End Game', height=size, width=25, command=m.destroy)
 stop.grid(row=3,column=0, columnspan = 10)
 
 
